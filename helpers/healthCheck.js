17af94af8462de782acd41be277811d115bdea76
Bob Braun <bobbraunjedi@gmail.com>
2019-07-09 22:40:06 +0300

    update HC

--- helpers/healthCheck.js
+++ helpers/healthCheck.js
@@ -1,16 +1,16 @@
 const request = require('request');
 const _ = require('lodash');
+const log = require('./log');
 
 module.exports = (nodes) => {
 	if (typeof nodes === 'string') {
 		return () => nodes;
 	}
 
-	this.hotNode = nodes[0];
 	checkNodes(nodes, this);
-
+	this.hotNode = nodes[0];
 	setInterval(() => {
-		checkNodes(_.shuffle(nodes), this)
+		checkNodes(_.shuffle(nodes), this);
 	}, 60000);
 
 	return {
@@ -18,22 +18,24 @@ module.exports = (nodes) => {
 			return this.hotNode;
 		},
 		changeNodes: () => {
-			console.log('Health check warn: Force change node!');
+			log.warn('[Health check]: Force change node!');
 			checkNodes(_.shuffle(nodes), this);
 		}
 	};
 };
 
 function checkNodes(nodes, context) {
+	let isFind = false;
 	nodes.forEach(async n => {
 		const res = await checkNode(n + '/api/peers/version');
 		if (res) {
+			isFind = true;
 			context.hotNode = n;
-		} else {
-			// console.log('Error health check ' + n);
 		}
 	});
-	// console.log('Health check update node: ', context.hotNode);
+	if (!isFind && context.hotNode) {
+		log.error('[Health check]: ALL nodes don`t ping! Pls check you internet connection!');
+	}
 }
 
 function checkNode(url) {
