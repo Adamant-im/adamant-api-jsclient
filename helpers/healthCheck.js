9a43263bd0bbdf4bcd1b09fb5e201976d1d706a9
Bob Braun <bobbraunjedi@gmail.com>
2019-07-11 20:56:01 +0300

    add check heigth in hc

--- helpers/healthCheck.js
+++ helpers/healthCheck.js
@@ -5,12 +5,11 @@ module.exports = (nodes, log) => {
 	if (typeof nodes === 'string') {
 		return () => nodes;
 	}
-
-	checkNodes(nodes, this, log);
 	this.hotNode = nodes[0];
+	checkNodes(nodes, this, log);
 	setInterval(() => {
 		checkNodes(_.shuffle(nodes), this, log);
-	}, 60000);
+	}, 10000);
 
 	return {
 		node: () => {
@@ -24,20 +23,50 @@ module.exports = (nodes, log) => {
 };
 
 async function checkNodes(nodes, context, log) {
-	let isFind = false;
-	for (let n of nodes) {
-		const res = await checkNode(n + '/api/peers/version');
-		if (res) {
-			isFind = true;
-			context.hotNode = n;
-		} else {
-			console.log('Error check', n, !!res);
+	const health = [];
+	nodes.forEach(async n => {
+		try {
+			const start = unix();
+			const height = await checkNode(n + '/api/node/status');
+			if (height) {
+				health.push({
+					node: n,
+					ping: unix() - start,
+					height: Math.round(height / 20)
+				});
+			} else {
+				console.log('Error check', n, height);
+			}
+		} catch (e) {}
+	});
+
+	setTimeout(() => {
+
+		const count = health.length;
+		if (!count) {
+			log.error('[Health check]: ALL nodes don`t ping! Pls check you internet connection!');
+			return;
 		}
-	};
-	if (!isFind && context.hotNode) {
-		log.error('[Health check]: ALL nodes don`t ping! Pls check you internet connection!');
-	}
-	// console.log('Finish');
+
+		if (count === 1) {
+			context.hotNode = health[0].node;
+		} else if (count === 2) {
+			const h0 = health[0];
+			const h1 = health[1];
+			context.hotNode = h0.height > h1.height ? h0.node : h1.node;
+		} else {
+			let biggestGroup = [];
+			const groups = _.groupBy(health, n => n.height);
+			Object.keys(groups).forEach(key => {
+				if (groups[key].length > biggestGroup.length){
+					biggestGroup = groups[key];
+				}
+			});
+			biggestGroup.sort((a, b) => a.ping - b.ping);
+			context.hotNode = biggestGroup[0].node;
+		}
+		console.log('hotNode', context.hotNode);
+	}, 3000);
 }
 
 function checkNode(url) {
@@ -45,9 +74,18 @@ function checkNode(url) {
 		request(url, (err, res, body) => {
 			if (err) {
 				resolve(false);
-			} else if (res.statusCode === 200) {
-				resolve(true);
+			} else {
+				try {
+					const heigth = JSON.parse(body).network.height;
+					resolve(heigth);
+				} catch (e) {
+					resolve(false);
+				}
 			}
 		});
 	});
 };
+
+function unix(){
+	return new Date().getTime();
+}
