368348ce9db860335a12ee8dd4ae98ae1cdee37b
Bob Braun <bobbraunjedi@gmail.com>
2019-07-17 19:42:18 +0300

    add parseFloat to rich amount

--- groups/send.js
+++ groups/send.js
@@ -3,6 +3,7 @@ const keys = require('../helpers/keys');
 const encrypter = require('../helpers/encrypter');
 const constants = require('../helpers/constants');
 const transactionFormer = require('../helpers/transactionFormer');
+const SAT = 100000000;
 
 module.exports = (hotNode, log) => {
 	return (passPhrase, address, payload, type = 'tokens', isEncode, amount_comment) => {
@@ -11,7 +12,7 @@ module.exports = (hotNode, log) => {
 
 		if (type === 'tokens') {
 			try {
-				const amount = parseInt(parseFloat(String(payload)) * 100000000);
+				const amount = parseInt(parseFloat(String(payload)) * SAT);
 				const data = {
 					keyPair,
 					recipientId,
@@ -33,14 +34,14 @@ module.exports = (hotNode, log) => {
 		} else if (['message', 'rich', 'signal'].includes(type)) {
 			try {
 				let message_type = 1;
-				
+
 				if (type === 'rich'){
 					message_type = 2;
 				};
 				if (type === 'signal'){
 					message_type = 3;
 				};
-				
+
 				const data = {
 					keyPair,
 					recipientId,
@@ -48,7 +49,7 @@ module.exports = (hotNode, log) => {
 				};
 
 				if (amount_comment) {
-					data.amount = +amount_comment * 100000000;
+					data.amount = parseInt(parseFloat(String(amount_comment)) * SAT);
 				}
 
 				const res = request('GET', hotNode() + '/api/accounts/getPublicKey?address=' + recipientId);
@@ -60,11 +61,11 @@ module.exports = (hotNode, log) => {
 					data.message = encrypt_data.message;
 					data.own_message = encrypt_data.own_message;
 					const transaction = transactionFormer.createTransaction(constants.transactionTypes.CHAT_MESSAGE, data);
-				
+
 					if (isEncode) {
 						return transaction;
 					}
-				
+
 					return JSON.parse(request('POST', hotNode() + '/api/transactions/process', {
 						json: {
 							transaction
