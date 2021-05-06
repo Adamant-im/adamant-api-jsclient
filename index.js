69d80fc895387992a50b8e9fecca06fec7443bb1
adamant-al <adamantim@yandex.ru>
2021-05-07 00:04:36 +0300

    Remove syncGet

--- index.js
+++ index.js
@@ -3,7 +3,6 @@ const decodeMsg = require('./groups/decodeMsg');
 const Send = require('./groups/send');
 const healthCheck = require('./helpers/healthCheck');
 const eth = require('./groups/eth');
-const syncGet = require('./groups/syncGet');
 const transactionFormer = require('./helpers/transactionFormer');
 const keys = require('./helpers/keys');
 const encrypter = require('./helpers/encrypter');
@@ -13,16 +12,13 @@ const logger = require('adamant-api/helpers/logger');
 module.exports = (params, log) => {
 	log = log || console;
 	logger.initLogger(params.logLevel, log);
-	const {node, changeNodes} = healthCheck(params.node);
 	const nodeManager = healthCheck(params.node);
-	const syncReq = syncGet(node, changeNodes);
 	
 	return {
 		get: get(nodeManager),
-		send: Send(node),
+		// send: Send(node),
 		decodeMsg,
 		eth,
-		syncGet: syncReq,
 		transactionFormer,
 		keys,
 		encrypter,
