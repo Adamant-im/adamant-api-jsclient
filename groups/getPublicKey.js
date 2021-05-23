bd2c6657994d90d79881b89198805f0d724a3db3
adamant-al <adamantim@yandex.ru>
2021-05-23 15:08:20 +0300

    Async getPublicKey()

--- groups/getPublicKey.js
+++ groups/getPublicKey.js
@@ -3,24 +3,26 @@ const logger = require('../helpers/logger');
 
 module.exports = (nodeManager) => {
 
-  return (address) => {
+  return async (address) => {
 
     try {
 
       const publicKey = await get(nodeManager)('/accounts/getPublicKey', { address });
       if (publicKey.success) {
         if (publicKey.result.success) {
-          console.log('publicKey.result', publicKey.result)
-          // return votesCount
+          return publicKey.result.publicKey
         } else {
           logger.warn(`Unable to get public key for ${address}. Node's reply: ${publicKey.result.error}.`);
+          return false
         }
       } else {
         logger.warn(`Failed to get public key for ${address}, ${publicKey.error}. Message: ${publicKey.message}.`);
+        return false
       }
 
     } catch (e) {
       logger.error(`Error while fetching public key for ${address}:`, e);
+      return false
     }
 
   }
