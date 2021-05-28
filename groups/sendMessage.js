766fa339a1f119e97e5253b065ed3914ca2fe4bb
adamant-al <adamantim@yandex.ru>
2021-05-28 09:53:54 +0300

    We can use public key instead of address to send message

--- groups/sendMessage.js
+++ groups/sendMessage.js
@@ -14,7 +14,9 @@ module.exports = (nodeManager) => {
 		* Creates Message transaction, signs it, and broadcasts to ADAMANT network. Supports Basic, Rich and Signal Message Types.
     * See https://github.com/Adamant-im/adamant/wiki/Message-Types
     * @param {string} passPhrase Senders's passPhrase. Sender's address will be derived from it.
-    * @param {string} addressOrPublicKey Recipient's ADAMANT address or public key. Using public key is faster, as the library wouldn't request it from the network
+    * @param {string} addressOrPublicKey Recipient's ADAMANT address or public key.
+    * Using public key is faster, as the library wouldn't request it from the network.
+    * Though we cache public keys, and next request with address will be processed as fast as with public key.
     * @param {string} message Message plain text in case of basic message. Stringified JSON in case of rich or signal messages.
     * Example of rich message for Ether in-chat transfer: `{"type":"eth_transaction","amount":"0.002","hash":"0xfa46d2b3c99878f1f9863fcbdb0bc27d220d7065c6528543cbb83ced84487deb","comments":"I like to send it, send it"}`
     * @param {string, number} message_type Type of message: basic, rich, or signal
@@ -23,7 +25,7 @@ module.exports = (nodeManager) => {
     * @param {number} maxRetries How much times to retry request
     * @returns {Promise} Request results
   	*/
-	return (passPhrase, addressOrPublicKey, message, message_type = 'basic', amount, isAmountInADM = true, maxRetries = DEFAULT_SEND_MESSAGE_RETRIES, retryNo = 0) => {
+	return async (passPhrase, addressOrPublicKey, message, message_type = 'basic', amount, isAmountInADM = true, maxRetries = DEFAULT_SEND_MESSAGE_RETRIES, retryNo = 0) => {
 
     let keyPair, data;
     let address, publicKey;
@@ -40,7 +42,11 @@ module.exports = (nodeManager) => {
           return validator.badParameter('addressOrPublicKey', addressOrPublicKey)
         } else {
           publicKey = addressOrPublicKey;
-          address = keys.createAddressFromPublicKey(publicKey)
+          try {
+            address = keys.createAddressFromPublicKey('publicKey')
+          } catch (e) {
+            return validator.badParameter('addressOrPublicKey', addressOrPublicKey)
+          }
         }
       } else {
         publicKey = '';
@@ -63,7 +69,7 @@ module.exports = (nodeManager) => {
 
       data = {
         keyPair,
-        recipientId: addressOrPublicKey,
+        recipientId: address,
         message_type
       };
   
@@ -84,60 +90,54 @@ module.exports = (nodeManager) => {
 
     }
 
-    return getPublicKey(nodeManager)(addressOrPublicKey)
-      .then((publicKey) => {
+    if (!publicKey)
+      publicKey = await getPublicKey(nodeManager)(address);
 
-        if (publicKey) {
+    if (!publicKey)
+      return new Promise((resolve, reject) => {
+        resolve({
+          success: false,
+          errorMessage: `Unable to get public key for ${addressOrPublicKey}. It is necessary for sending an encrypted message. Account may be uninitialized (https://medium.com/adamant-im/chats-and-uninitialized-accounts-in-adamant-5035438e2fcd), or network error`
+        })
+      })
 
-          try {
+    try {
 
-            const encryptedMessage = encryptor.encodeMessage(message, keyPair, publicKey);
-            data.message = encryptedMessage.message;
-            data.own_message = encryptedMessage.own_message;
+      const encryptedMessage = encryptor.encodeMessage(message, keyPair, publicKey);
+      data.message = encryptedMessage.message;
+      data.own_message = encryptedMessage.own_message;
 
-            let transaction = transactionFormer.createTransaction(constants.transactionTypes.CHAT_MESSAGE, data);
+      let transaction = transactionFormer.createTransaction(constants.transactionTypes.CHAT_MESSAGE, data);
 
-            let url = nodeManager.node() + '/api/transactions/process';
-            return axios.post(url, { transaction })
-              .then(function (response) {
-                return validator.formatRequestResults(response, true)
+      let url = nodeManager.node() + '/api/transactions/process';
+      return axios.post(url, { transaction })
+        .then(function (response) {
+          return validator.formatRequestResults(response, true)
+        })
+        .catch(function (error) {
+          let logMessage = `[ADAMANT js-api] Send message request: Request to ${url} failed with ${error.response ? error.response.status : undefined} status code, ${error.toString()}${error.response && error.response.data ? '. Message: ' + error.response.data.toString().trim() : ''}. Try ${retryNo+1} of ${maxRetries+1}.`;
+          if (retryNo < maxRetries) {
+            logger.log(`${logMessage} Retrying…`);
+            return nodeManager.changeNodes()
+              .then(function () {
+                return module.exports(nodeManager)(passPhrase, addressOrPublicKey, message, message_type, tokensAmount, maxRetries, ++retryNo)
               })
-              .catch(function (error) {
-                let logMessage = `[ADAMANT js-api] Send message request: Request to ${url} failed with ${error.response ? error.response.status : undefined} status code, ${error.toString()}${error.response && error.response.data ? '. Message: ' + error.response.data.toString().trim() : ''}. Try ${retryNo+1} of ${maxRetries+1}.`;
-                if (retryNo < maxRetries) {
-                  logger.log(`${logMessage} Retrying…`);
-                  return nodeManager.changeNodes()
-                    .then(function () {
-                      return module.exports(nodeManager)(passPhrase, addressOrPublicKey, message, message_type, tokensAmount, maxRetries, ++retryNo)
-                    })
-                }
-                logger.warn(`${logMessage} No more attempts, returning error.`);
-                return validator.formatRequestResults(error, false)
-              })
-              
-          } catch (e) {
-
-            return new Promise((resolve, reject) => {
-              resolve({
-                success: false,
-                errorMessage: `Unable to encode message '${message}' with public key ${publicKey}, or unable to build a transaction. Exception: ` + e
-              })
-            })
-
           }
+          logger.warn(`${logMessage} No more attempts, returning error.`);
+          return validator.formatRequestResults(error, false)
+        })
+        
+    } catch (e) {
 
-        } else {
+      return new Promise((resolve, reject) => {
+        resolve({
+          success: false,
+          errorMessage: `Unable to encode message '${message}' with public key ${publicKey}, or unable to build a transaction. Exception: ` + e
+        })
+      })
 
-          return new Promise((resolve, reject) => {
-            resolve({
-              success: false,
-              errorMessage: `Unable to get public key for ${addressOrPublicKey}. It is necessary for sending an encrypted message. Account may be uninitialized (https://medium.com/adamant-im/chats-and-uninitialized-accounts-in-adamant-5035438e2fcd), or network error`
-            })
-          })
+    }
 
-        } // if (publicKey)
-
-      }) // getPublicKey.then
-  }
+  } // sendMessage()
 
 };
