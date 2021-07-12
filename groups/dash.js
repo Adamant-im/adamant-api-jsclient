156f37c3d9677237a13b20262bde7316bd74affb
adamant-al <adamantim@yandex.ru>
2021-07-12 12:33:33 +0300

    Update Dash keys

--- groups/dash.js
+++ groups/dash.js
@@ -5,24 +5,21 @@ const dash = { }
 /**
  * Generates a DASH account from the passphrase specified.
  * @param {string} passphrase ADAMANT account passphrase
- * @returns {{address: String, privateKey: Buffer}}
+ * @returns {object} network info, keyPair, privateKey, privateKeyWIF
  */
 
 dash.keys = passphrase => {
   const network = coinNetworks.DASH;
   const pwHash = bitcoin.crypto.sha256(Buffer.from(passphrase));
   const keyPair = bitcoin.ECPair.fromPrivateKey(pwHash, { network });
-  const asWif = false; // DASH private key is not of Wallet Import Format
-  const privateKey = asWif
-    ? keyPair.toWIF()
-    : keyPair.privateKey.toString('hex')
 
   return {
     network,
     keyPair,
     address: bitcoin.payments.p2pkh({ pubkey: keyPair.publicKey, network }).address,
-    privateKey,
-    privateKeyWIF: keyPair.toWIF()
+    // DASH private key is a regular 256-bit key
+    privateKey: keyPair.privateKey.toString('hex'), // regular 256-bit (32 bytes, 64 characters) private key
+    privateKeyWIF: keyPair.toWIF() // Wallet Import Format (52 base58 characters)
   }
 
 };
