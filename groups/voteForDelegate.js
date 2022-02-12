97d925d70a0e9c22d4a4340589f93d4223bd3208
martiliones <hi@martilion.es>
2022-02-12 12:44:34 +0600

    Update voteForDelegate() description

--- groups/voteForDelegate.js
+++ groups/voteForDelegate.js
@@ -15,7 +15,8 @@ module.exports = (nodeManager) => {
 		* Creates votes for delegate transaction, signs it, and broadcasts to ADAMANT network
     * See https://github.com/Adamant-im/adamant/wiki/Transaction-Types#type-3-vote-for-delegate-transaction
     * @param {string} passPhrase Senders's passPhrase. Sender's address will be derived from it.
-    * @param {string[]} votes PublicKeys, ADM addresses and delegate names for upvote and downvote
+    * @param {string[]} votes PublicKeys, ADM addresses and delegate names for upvote and downvote.
+    * It would be more efficient to pass publicKey, otherwise the api will make additional queries
     * @param {number} maxRetries How much times to retry request
     * @returns {Promise} Request results
   	*/
