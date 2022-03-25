9255312b143e1789ac9fa46c1cdccf99b45492e0
martiliones <hi@martilion.es>
2022-03-25 19:40:16 +0600

    refactor(validator): fix ESLint errors and simplify

--- src/helpers/validator.js
+++ src/helpers/validator.js
@@ -1,136 +1,112 @@
 const constants = require('./constants');
-const BigNumber = require('bignumber.js');
+const bigNumber = require('bignumber.js');
 
 module.exports = {
+  getRandomIntInclusive(minimum, maximum) {
+    const min = Math.ceil(minimum);
+    const max = Math.floor(maximum);
 
-  getRandomIntInclusive(min, max) {
-    min = Math.ceil(min);
-    max = Math.floor(max);
-    return Math.floor(Math.random() * (max - min + 1) + min); // The maximum is inclusive and the minimum is inclusive
+    // The maximum is inclusive and the minimum is inclusive
+    return Math.floor(Math.random() * (max - min + 1) + min);
   },
-
   isNumeric(str) {
-    if (typeof str !== 'string') return false;
-    return !isNaN(str) && !isNaN(parseFloat(str));
-  },
+    if (typeof str !== 'string') {
+      return false;
+    }
 
+    return !isNaN(parseFloat(str));
+  },
   tryParseJSON(jsonString) {
     try {
       const o = JSON.parse(jsonString);
-      if (o && typeof o === 'object') {
-        return o;
-      }
-    } catch (e) { }
-    return false;
-  },
 
+      return typeof o === 'object' ? o : false;
+    } catch (e) {
+      return false;
+    }
+  },
   validatePassPhrase(passPhrase) {
-    if (!passPhrase || typeof(passPhrase) !== 'string' || passPhrase.length < 30) {
-      return false;
-    } else {
-      return true;
-    }
+    return typeof passPhrase === 'string' && passPhrase.length > 30;
   },
-
   validateEndpoint(endpoint) {
-    if (!endpoint || typeof(endpoint) !== 'string') {
-      return false;
-    } else {
-      return true;
-    }
+    return typeof endpoint === 'string';
   },
-
   validateAdmAddress(address) {
-    if (!address || typeof(address) !== 'string' || !constants.RE_ADM_ADDRESS.test(address)) {
-      return false;
-    } else {
-      return true;
-    }
+    return typeof(address) === 'string' && constants.RE_ADM_ADDRESS.test(address);
   },
-
   validateAdmPublicKey(publicKey) {
-    if (!publicKey || typeof(publicKey) !== 'string' || publicKey.length !== 64 || !constants.RE_HEX.test(publicKey)) {
-      return false;
-    } else {
-      return true;
-    }
+    return (
+      typeof publicKey === 'string' &&
+      publicKey.length === 64 &&
+      constants.RE_HEX.test(publicKey)
+    );
   },
-
   validateAdmVoteForPublicKey(publicKey) {
-    return (publicKey && typeof(publicKey) === 'string' && constants.RE_ADM_VOTE_FOR_PUBLIC_KEY.test(publicKey));
+    return (
+      typeof publicKey === 'string' &&
+      constants.RE_ADM_VOTE_FOR_PUBLIC_KEY.test(publicKey)
+    );
   },
-
   validateAdmVoteForAddress(address) {
-    return (address && typeof(address) === 'string' && constants.RE_ADM_VOTE_FOR_ADDRESS.test(address));
+    return (
+      typeof address === 'string' &&
+      constants.RE_ADM_VOTE_FOR_ADDRESS.test(address)
+    );
   },
-
   validateAdmVoteForDelegateName(delegateName) {
-    return (delegateName && typeof(delegateName) === 'string' && constants.RE_ADM_VOTE_FOR_DELEGATE_NAME.test(delegateName));
+    return (
+      typeof delegateName === 'string' &&
+      constants.RE_ADM_VOTE_FOR_DELEGATE_NAME.test(delegateName)
+    );
   },
-
   validateIntegerAmount(amount) {
-    if (!amount || typeof(amount) !== 'number' || isNaN(amount) || !Number.isSafeInteger(amount)) {
-      return false;
-    } else {
-      return true;
-    }
+    return Number.isSafeInteger(amount);
   },
-
   validateStringAmount(amount) {
-    if (!amount || !this.isNumeric(amount)) {
-      return false;
-    } else {
-      return true;
-    }
+    return this.isNumeric(amount);
   },
-
-  validateMessageType(message_type) {
-    if (!message_type || typeof(message_type) !== 'number' || ![1, 2, 3].includes(message_type)) {
-      return false;
-    } else {
-      return true;
-    }
+  validateMessageType(messageType) {
+    return [1, 2, 3].includes(messageType);
   },
-
-  validateMessage(message, message_type) {
-    if (typeof(message) !== 'string') {
+  validateMessage(message, messageType) {
+    if (typeof message !== 'string') {
       return {
         result: false,
         error: `Message must be a string`,
       };
-    } else {
-      if (message_type === 2 || message_type === 3) {
-        const json = this.tryParseJSON(message);
+    }
 
-        if (!json) {
+    if ([2, 3].includes(messageType)) {
+      const json = this.tryParseJSON(message);
+
+      if (!json) {
+        return {
+          result: false,
+          error: `For rich and signal messages, 'message' must be a JSON string`,
+        };
+      }
+
+      if (json.type && json.type.toLowerCase().includes('_transaction')) {
+        if (json.type.toLowerCase() !== json.type) {
           return {
             result: false,
-            error: `For rich and signal messages, 'message' must be a JSON string`,
-          };
-        }
-
-        if (json.type && json.type.toLowerCase().includes('_transaction')) {
-          if (json.type.toLowerCase() !== json.type) {
-            return {
-              result: false,
-              error: `Value '<coin>_transaction' must be in lower case`,
-            };
-          }
-        }
-
-        if (typeof json.amount !== 'string' || !this.validateStringAmount(json.amount)) {
-          return {
-            result: false,
-            error: `Field 'amount' must be a string, representing a number`,
+            error: `Value '<coin>_transaction' must be in lower case`,
           };
         }
       }
+
+      if (typeof json.amount !== 'string' || !this.validateStringAmount(json.amount)) {
+        return {
+          result: false,
+          error: `Field 'amount' must be a string, representing a number`,
+        };
+      }
     }
+
     return {
       result: true,
     };
   },
-
   validateDelegateName(name) {
     if (typeof name !== 'string') {
       return false;
@@ -138,45 +114,56 @@ module.exports = {
 
     return constants.RE_ADM_DELEGATE_NAME.test(name);
   },
-
   AdmToSats(amount) {
-    return BigNumber(String(amount)).multipliedBy(constants.SAT).integerValue().toNumber();
+    return bigNumber(String(amount))
+        .multipliedBy(constants.SAT)
+        .integerValue()
+        .toNumber();
   },
-
-  badParameter(name, value, customMessage) {
-    return new Promise((resolve, reject) => {
-      resolve({
-        success: false,
-        errorMessage: `Wrong '${name}' parameter${value ? ': ' + value : ''}${customMessage ? '. Error: ' + customMessage : ''}`,
-      });
-    });
+  async badParameter(name, value, customMessage) {
+    return {
+      success: false,
+      errorMessage: `Wrong '${name}' parameter${value ? ': ' + value : ''}${customMessage ? '. Error: ' + customMessage : ''}`,
+    };
   },
-
   formatRequestResults(response, isRequestSuccess) {
-    const results = {};
-    results.details = {};
+    let results = {
+      details: {},
+    };
 
     if (isRequestSuccess) {
-      results.success = response.data && response.data.success;
-      results.data = response.data;
-      results.details.status = response.status;
-      results.details.statusText = response.statusText;
-      results.details.response = response;
+      results = {
+        success: response.data && response.data.success,
+        data: response.data,
+        details: {
+          status: response.status,
+          statusText: response.statusText,
+          response: response,
+        },
+      };
+
       if (!results.success && results.data) {
         results.errorMessage = `Node's reply: ${results.data.error}`;
       }
     } else {
-      results.success = false;
-      results.data = undefined;
-      results.details.status = response.response ? response.response.status : undefined;
-      results.details.statusText = response.response ? response.response.statusText : undefined;
-      results.details.error = response.toString();
-      results.details.message = response.response && response.response.data ? response.response.data.toString().trim() : undefined;
-      results.details.response = response.response;
-      results.errorMessage = `${results.details.error}${results.details.message ? '. Message: ' + results.details.message : ''}`;
+      results = {
+        success: false,
+        data: undefined,
+        details: {
+          status: response.status,
+          statusText: response.response && response.response.statusText,
+          response: response.response && response.response.status,
+          error: response.toString(),
+          message: (
+            response.response &&
+            response.response.data &&
+            response.response.data.toString().trim()
+          ),
+          errorMessage: `${results.details.error}${results.details.message ? '. Message: ' + results.details.message : ''}`,
+        },
+      };
     }
 
     return results;
   },
-
 };
