629b92ed5600812f813fb79ea05dbcca099fd68a
martiliones <hi@martilion.es>
2022-03-27 02:39:49 +0600

    fix(healthCheck): variables activeNode and isCheckingNodes do not update

--- src/helpers/healthCheck.js
+++ src/helpers/healthCheck.js
@@ -11,9 +11,10 @@ const CHECK_NODES_INTERVAL = 60 * 5 * 1000; // Update active nodes every 5 minut
 const HEIGHT_EPSILON = 5; // Used to group nodes by height and choose synced
 
 module.exports = (nodes, checkHealthAtStartup = true) => {
-  const isCheckingNodes = false;
   const nodesList = nodes;
-  const activeNode = nodesList[0]; // Note: it may be not synced; and before first health check a node can reply with obsolete data
+  let isCheckingNodes = false;
+  let liveNodes = [];
+  let activeNode = nodesList[0]; // Note: it may be not synced; and before first health check a node can reply with obsolete data
 
   /**
     * Updates active nodes. If nodes are already updating, returns Promise of previous call
@@ -30,6 +31,136 @@ module.exports = (nodes, checkHealthAtStartup = true) => {
 
       return true;
     }
+  }
+
+  /**
+    * Requests every ADAMANT node for its status, makes a list of live nodes, and chooses one active
+    * @param {boolean} forceChangeActiveNode
+    */
+  async function checkNodes(forceChangeActiveNode) {
+    isCheckingNodes = true;
+    liveNodes = [];
+
+    try {
+      for (const node of nodesList) {
+        try {
+          const start = unixTimestamp();
+
+          const req = await checkNode(`${node}/api/node/status`);
+
+          const [url] = node.replace(RE_HTTP_URL, '$1').split(':');
+          const ifIP = RE_IP.test(url);
+
+          const ip = ifIP ? url : await getIP(url);
+          const ifHttps = node.startsWith('https');
+
+          if (req.status) {
+            liveNodes.push({
+              node,
+              ifIP,
+              url,
+              ip,
+              ifHttps,
+              outOfSync: false,
+              ping: unixTimestamp() - start,
+              height: req.status.network.height,
+              heightEpsilon: Math.round(req.status.network.height / HEIGHT_EPSILON),
+              socketSupport: req.status.wsClient?.enabled,
+              wsPort: req.status.wsClient?.port,
+            });
+          } else {
+            logger.log(`[ADAMANT js-api] Health check: Node ${node} haven't returned its status`);
+          }
+        } catch (e) {
+          logger.log(`[ADAMANT js-api] Health check: Error while checking node ${node}, ${e}`);
+        }
+      }
+
+      const count = liveNodes.length;
+
+      let outOfSyncCount = 0;
+
+      if (!count) {
+        logger.error(`[ADAMANT js-api] Health check: All of ${nodesList.length} nodes are unavailable. Check internet connection and nodes list in config.`);
+      } else {
+        // Set activeNode to one that have maximum height and minimum ping
+        if (count === 1) {
+          activeNode = liveNodes[0].node;
+        } else if (count === 2) {
+          const [h0, h1] = liveNodes;
+
+          activeNode = h0.height > h1.height ? h0.node : h1.node;
+
+          // Mark node outOfSync if needed
+          if (h0.heightEpsilon > h1.heightEpsilon) {
+            liveNodes[1].outOfSync = true;
+            outOfSyncCount += 1;
+          } else if (h0.heightEpsilon < h1.heightEpsilon) {
+            liveNodes[0].outOfSync = true;
+            outOfSyncCount += 1;
+          }
+        } else {
+          let biggestGroup = [];
+          // Removing lodash: const groups = _.groupBy(liveNodes, n => n.heightEpsilon);
+          const groups = liveNodes.reduce((grouped, node) => {
+            const int = Math.floor(node.heightEpsilon); // Excessive, it is already rounded
+
+            if (!Object.prototype.hasOwnProperty.call(grouped, int)) {
+              grouped[int] = [];
+            }
+
+            grouped[int].push(node);
+
+            return grouped;
+          }, {});
+
+          Object.keys(groups).forEach((key) => {
+            if (groups[key].length > biggestGroup.length) {
+              biggestGroup = groups[key];
+            }
+          });
+
+          // All the nodes from the biggestGroup list are considered to be in sync, all the others are not
+          liveNodes.forEach((node) => {
+            node.outOfSync = !biggestGroup.includes(node);
+          });
+
+          outOfSyncCount = liveNodes.length - biggestGroup.length;
+
+          biggestGroup.sort((a, b) => a.ping - b.ping);
+          liveNodes.sort((a, b) => a.ping - b.ping);
+
+          if (forceChangeActiveNode && biggestGroup.length > 1 && activeNode === biggestGroup[0].node) {
+            // Use random node from which are synced
+            activeNode = biggestGroup[validator.getRandomIntInclusive(1, biggestGroup.length - 1)].node;
+          } else {
+            // Use node with minimum ping among which are synced
+            activeNode = biggestGroup[0].node;
+          }
+        }
+
+        socket.reviseConnection(liveNodes);
+
+        const unavailableCount = nodesList.length - liveNodes.length;
+        const supportedCount = liveNodes.length - outOfSyncCount;
+
+        let nodesInfoString = '';
+
+        if (unavailableCount) {
+          nodesInfoString += `, ${unavailableCount} nodes didn't respond`;
+        }
+
+        if (outOfSyncCount) {
+          nodesInfoString += `, ${outOfSyncCount} nodes are not synced`;
+        }
+
+        logger.log(`[ADAMANT js-api] Health check: Found ${supportedCount} supported and synced nodes${nodesInfoString}. Active node is ${activeNode}.`);
+      }
+    } catch (e) {
+      logger.warn('[ADAMANT js-api] Health check: Error in checkNodes(), ' + e);
+    }
+
+    isCheckingNodes = false;
   }
 
   if (checkHealthAtStartup) {
@@ -50,136 +181,6 @@ module.exports = (nodes, checkHealthAtStartup = true) => {
   };
 };
 
-/**
-  * Requests every ADAMANT node for its status, makes a list of live nodes, and chooses one active
-  * @param {boolean} forceChangeActiveNode
-  */
-async function checkNodes(forceChangeActiveNode) {
-  this.isCheckingNodes = true;
-  this.liveNodes = [];
-
-  try {
-    for (const node of this.nodesList) {
-      try {
-        const start = unixTimestamp();
-
-        const req = await checkNode(`${node}/api/node/status`);
-
-        const [url] = node.replace(RE_HTTP_URL, '$1').split(':');
-        const ifIP = RE_IP.test(url);
-
-        const ip = ifIP ? url : await getIP(url);
-        const ifHttps = node.startsWith('https');
-
-        if (req.status) {
-          this.liveNodes.push({
-            node,
-            ifIP,
-            url,
-            ip,
-            ifHttps,
-            outOfSync: false,
-            ping: unixTimestamp() - start,
-            height: req.status.network.height,
-            heightEpsilon: Math.round(req.status.network.height / HEIGHT_EPSILON),
-            socketSupport: req.status.wsClient?.enabled,
-            wsPort: req.status.wsClient?.port,
-          });
-        } else {
-          logger.log(`[ADAMANT js-api] Health check: Node ${node} haven't returned its status`);
-        }
-      } catch (e) {
-        logger.log(`[ADAMANT js-api] Health check: Error while checking node ${node}, ${e}`);
-      }
-    }
-
-    const count = this.liveNodes.length;
-
-    let outOfSyncCount = 0;
-
-    if (!count) {
-      logger.error(`[ADAMANT js-api] Health check: All of ${this.nodesList.length} nodes are unavailable. Check internet connection and nodes list in config.`);
-    } else {
-      // Set activeNode to one that have maximum height and minimum ping
-      if (count === 1) {
-        this.activeNode = this.liveNodes[0].node;
-      } else if (count === 2) {
-        const [h0, h1] = this.liveNodes;
-
-        this.activeNode = h0.height > h1.height ? h0.node : h1.node;
-
-        // Mark node outOfSync if needed
-        if (h0.heightEpsilon > h1.heightEpsilon) {
-          this.liveNodes[1].outOfSync = true;
-          outOfSyncCount += 1;
-        } else if (h0.heightEpsilon < h1.heightEpsilon) {
-          this.liveNodes[0].outOfSync = true;
-          outOfSyncCount += 1;
-        }
-      } else {
-        let biggestGroup = [];
-        // Removing lodash: const groups = _.groupBy(this.liveNodes, n => n.heightEpsilon);
-        const groups = this.liveNodes.reduce((grouped, node) => {
-          const int = Math.floor(node.heightEpsilon); // Excessive, it is already rounded
-
-          if (!Object.prototype.hasOwnProperty.call(grouped, int)) {
-            grouped[int] = [];
-          }
-
-          grouped[int].push(node);
-
-          return grouped;
-        }, {});
-
-        Object.keys(groups).forEach((key) => {
-          if (groups[key].length > biggestGroup.length) {
-            biggestGroup = groups[key];
-          }
-        });
-
-        // All the nodes from the biggestGroup list are considered to be in sync, all the others are not
-        this.liveNodes.forEach((node) => {
-          node.outOfSync = !biggestGroup.includes(node);
-        });
-
-        outOfSyncCount = this.liveNodes.length - biggestGroup.length;
-
-        biggestGroup.sort((a, b) => a.ping - b.ping);
-        this.liveNodes.sort((a, b) => a.ping - b.ping);
-
-        if (forceChangeActiveNode && biggestGroup.length > 1 && this.activeNode === biggestGroup[0].node) {
-          // Use random node from which are synced
-          this.activeNode = biggestGroup[validator.getRandomIntInclusive(1, biggestGroup.length - 1)].node;
-        } else {
-          // Use node with minimum ping among which are synced
-          this.activeNode = biggestGroup[0].node;
-        }
-      }
-
-      socket.reviseConnection(this.liveNodes);
-
-      const unavailableCount = this.nodesList.length - this.liveNodes.length;
-      const supportedCount = this.liveNodes.length - outOfSyncCount;
-
-      let nodesInfoString = '';
-
-      if (unavailableCount) {
-        nodesInfoString += `, ${unavailableCount} nodes didn't respond`;
-      }
-
-      if (outOfSyncCount) {
-        nodesInfoString += `, ${outOfSyncCount} nodes are not synced`;
-      }
-
-      logger.log(`[ADAMANT js-api] Health check: Found ${supportedCount} supported and synced nodes${nodesInfoString}. Active node is ${this.activeNode}.`);
-    }
-  } catch (e) {
-    logger.warn('[ADAMANT js-api] Health check: Error in checkNodes(), ' + e);
-  }
-
-  this.isCheckingNodes = false;
-}
-
 async function getIP(url) {
   try {
     const addresses = await dnsPromises.resolve4(url);
@@ -199,12 +200,8 @@ async function getIP(url) {
   */
 function checkNode(url) {
   return axios.get(url)
-      .then(function(response) {
-        return {status: response.data};
-      })
-      .catch(function(error) {
-        return false;
-      });
+      .then((response) => ({status: response.data}))
+      .catch((err) => false);
 }
 
 function unixTimestamp() {
