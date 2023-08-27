a135a0fb9a036fc154c10ef49da384e140a6a765
martiliones <hi@martilion.es>
2023-08-27 23:00:25 +0600

    fix: eslint erros and minor changes

--- src/groups/eth.js
+++ src/groups/eth.js
@@ -1,15 +1,16 @@
 const {mnemonicToSeedSync} = require('bip39');
 const hdkey = require('hdkey');
-const HD_KEY_PATH = 'm/44\'/60\'/3\'/1/0';
 const {bufferToHex, privateToAddress} = require('ethereumjs-util');
-const eth = { };
+
+const HD_KEY_PATH = 'm/44\'/60\'/3\'/1/0';
+
+const eth = {};
 
 /**
  * Generates a ETH account from the passphrase specified.
  * @param {string} passphrase ADAMANT account passphrase
- * @returns {{address: String, privateKey: Buffer}}
+ * @return {{address: String, privateKey: Buffer}}
  */
-
 eth.keys = (passphrase) => {
   const seed = mnemonicToSeedSync(passphrase);
   const privateKey = hdkey.fromMasterSeed(seed).derive(HD_KEY_PATH)._privateKey;
