00163e3f34725b5447338498aef854fbd53bc8a4
martiliones <hi@martilion.es>
2023-12-01 16:39:55 +0600

    feat: export validator utils

--- src/index.ts
+++ src/index.ts
@@ -6,3 +6,14 @@ export * from './helpers/constants';
 export * from './helpers/logger';
 export * from './helpers/wsClient';
 export * from './helpers/keys';
+export {
+  isPassphrase,
+  isAdmAddress,
+  isAdmPublicKey,
+  isAdmVoteForPublicKey,
+  isAdmVoteForAddress,
+  isAdmVoteForDelegateName,
+  validateMessage,
+  isDelegateName,
+  admToSats,
+} from './helpers/validator';
