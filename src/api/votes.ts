73309a6368267ba8b5108391784465771d9ebd77
martiliones <hi@martilion.es>
2024-03-18 13:05:51 +0600

    v2.3.0
    * feat: export validator utils
    
    * chore: bump a version
    
    * fix(typescript): represent transaction id as a string (#52)
    
    * fix: remove preinstall npm script (#53)
    
    * fix: remove preinstall npm script
    
    * chore: bump a version
    
    * Fix/transaction query (#51)
    
    * chore(CHANGELOG): v2.3.0

--- src/api/votes.ts
+++
@@ -1,8 +0,0 @@
-export type VoteDirection = '+' | '-';
-
-export const parseVote = (vote: string): [string, VoteDirection] => {
-  const name = vote.slice(1);
-  const direction = vote.charAt(0);
-
-  return [name, direction as VoteDirection];
-};
